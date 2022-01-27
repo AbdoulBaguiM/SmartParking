2783240dbed4a017cd3f26067deed583f771cc59
Abdelhk <72513211+abdoulbaguimohamadou@users.noreply.github.com>
2022-01-27 22:32:20 +0100

    Login + Register

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,160 @@
+package controllers;
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import utils.ConnectionUtil;
+
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private Label lblErrors;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private Button btnSignin;
+
+    
+    Connection con = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    
+    @FXML
+    public void handleButtonAction(MouseEvent event) {
+
+        if (event.getSource() == btnSignin) {
+            //login here
+            if (logIn().equals("Success")) {
+                try {
+
+                    //add you loading or delays - ;-)
+                    Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    //stage.setMaximized(true);
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/OnBoard.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+
+            }
+        }
+/*        if (event.getSource() == btnSignup) {
+            //login here
+            try {
+
+                //add you loading or delays - ;-)
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+                //stage.setMaximized(true);
+                stage.close();
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/Register.fxml")));
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }*/
+    }
+    
+    @FXML
+    private void switchViewButton (ActionEvent event){
+        try {
+
+            //add you loading or delays - ;-)
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            //stage.setMaximized(true);
+            stage.close();
+            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/Register.fxml")));
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        if (con == null) {
+            lblErrors.setTextFill(Color.TOMATO);
+            lblErrors.setText("Connection au serveur : Echec");
+        } else {
+            lblErrors.setTextFill(Color.GREEN);
+            lblErrors.setText("Connection au serveur : Succes");
+        }
+    }
+
+    public LoginController() {
+        con = ConnectionUtil.conDB();
+    }
+
+    //we gonna use string to check for status
+    private String logIn() {
+        String status = "Success";
+        String email = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        if(email.isEmpty() || password.isEmpty()) {
+            setLblError(Color.TOMATO, "Veuillez remplir tous les champs");
+            status = "Error";
+        } else {
+            //query
+            String sql = "SELECT * FROM admins Where email = ? and password = ?";
+            try {
+                preparedStatement = con.prepareStatement(sql);
+                preparedStatement.setString(1, email);
+                preparedStatement.setString(2, password);
+                resultSet = preparedStatement.executeQuery();
+                if (!resultSet.next()) {
+                    setLblError(Color.TOMATO, "L'identifiant ou le mot de passe est incorrect");
+                    status = "Error";
+                } else {
+                    setLblError(Color.GREEN, "Connexion réussie, redirection en cours");
+                }
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+                status = "Exception";
+            }
+        }
+
+        return status;
+    }
+
+    private void setLblError(Color color, String text) {
+        lblErrors.setTextFill(color);
+        lblErrors.setText(text);
+        System.out.println(text);
+    }
+}
---
+++ src/home/Main.java
@@ -0,0 +1,61 @@
+package home;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Main extends Application {
+
+    //define your offsets here
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Register.fxml"));
+  
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setMaximized(false);
+
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        
+        Image icon = new Image(getClass().getResourceAsStream("/images/logo2.png"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Smart Parking");
+        stage.getIcons().add(icon);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
